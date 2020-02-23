f9d270c8c791787224687a03e30eb2f48ead7be4
kilkuwilku <56803463+kilkuwilku@users.noreply.github.com>
2020-02-23 17:05:00 +0100

    catch err function added

--- weather/js/app.js
+++ weather/js/app.js
@@ -2,10 +2,11 @@ const btn = document.getElementById('button');
 const input = document.getElementById('city');
 const key = '059bf24e572d7d97492b46aa2658101c';
 
-btn.addEventListener('click', function () {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + input.value + '&appid=' + key + '&units=metric')  
-    .then(function(resp) {return resp.json()})
-    .then(function(data) {
+btn.addEventListener('click', () => {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + input.value + '&appid=' + key + '&units=metric')
+    .then((resp) => {return resp.json()})
+    .then((data) => {
+      
       //collect necessary data
       let city = data['name'];
       let temp = data['main']['temp'];
@@ -15,6 +16,8 @@ btn.addEventListener('click', function () {
       let sky = data['weather']['0']['description'];
       let ico = data['weather']['0']['icon'];
 
+
+      
       //print into index
       document.getElementById('content').innerHTML = `<div id = 'weather'>
       <h1>${city}</h1><img src = "http://openweathermap.org/img/wn/${ico}@2x.png" id = "ico"/>
@@ -24,14 +27,11 @@ btn.addEventListener('click', function () {
       <p>Pressure: ${pressure} hPa </p>
       <p>Sky: ${sky}</p>
       </div>`;
+      
       //animate results
       document.getElementById('results').style.transition = 'opacity 1s ease-in';
       document.getElementById('results').style.opacity='1';
-    })
-    
-    .catch(function() {
-      // catch any errors
+      })
+      
+      .catch(error => alert(`${input.value} not found ////   ${error}`));
     });
-  }
-);
-
